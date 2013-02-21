39a6417e3f7adddced0c3b80000f38dfdaec3946
NLDK <dankirlin@gmail.com>
2013-02-20 22:54:15 -0500

    added boundary conditions for calculating utility of shielded pieces, and added the utility functions for these shielded pieces

--- Utility.m
+++ Utility.m
@@ -1,41 +1,97 @@
 function [value] = Utility(S, RW)
 
-%two evaluation functions to make selection for white and red
+%two evcolumnlucolumntion functions to mcolumnke selection for white columnnd red
 value = 0;
 
 if (RW == 0)
-    for a=1:8
-        for b=1:8
-            if (mod(a+b,2)~=0)
+    for column=1:8
+        for row=1:8
+            if (mod(column+row,2)~=0)
                 weight = 0;
-                % Count pieces on board and assign weight values
-                if ( S(b,a)==1+RW || S(b,a)==3+RW) %friendly pawn piece
+                piece = S(row,column);
+                
+                %define pieces while obeying boundary conditions
+                if (row == 1 || row ==8 || column==1 || column ==8)
+                    
+                    %Top Left Boundary condition
+                    if( row ==1 || column ==1)
+                        pieceTopLeft = 0;
+                    else
+                        pieceTopLeft=S(row-1,column-1);
+                    end
+                    
+                    %Top Right boundary condition
+                    if ( row ==1 || column == 8)
+                        pieceTopRight = 0;
+                    else
+                        pieceTopRight = S(row-1, column+1);
+                    end
+                    
+                    %Bottom left boundary condition
+                    if (row == 8 || column ==1)
+                        pieceBottomLeft = 0;
+                    else
+                        pieceBottomLeft = S(row+1, column -1);
+                    end
+                    
+                    %bottom right boundary condition
+                    if ( row ==8 || column == 8)
+                        pieceBottomRight = 0;
+                    else
+                        pieceBottomRight = S(row+1,column+1);
+                    end
+                    
+                end
+            
+                % Count pieces and assign weight values
+                if ( piece ==1 || piece ==3) %friendly pawn piece
                     weight = 1;
                     
-                elseif (S(b,a) == 3)%friendly king piece
+                elseif (piece == 3)%friendly king piece
                     weight = 2;
                     
-                elseif ( S(b,a)==2+RW ) %enemy pawn piece
+                elseif ( piece == 2 ) %enemy pawn piece
                     weight = -1;
                 
-                elseif ( S(b,a)==4 ) %enemy king piece
+                elseif ( piece == 4 ) %enemy king piece
                     weight = -2;
                 end
 
                 %add utility for protected pieces
-                     %pieces on the side
-                     if ( (a ==1 || a==8 || b==1 || b==8) && (S(b,a) == 1 || S(b,a) == 3) )
+                    
+                    %pieces on the side
+                     if ( (column ==1 || column==8 || row==1 || row==8) && (piece == 1 || piece == 3) )
                          weight = 0.4 + weight;
-                     end  
-                     %pieces with a piece diagonally behind it
+                     end
+                     
+                     %pieces with column piece diagonally behind it
+                     if( (piece == 1 || piece ==3) &&(pieceTopLeft == 1 || pieceTopLeft ==3))
+                         weight = 0.2 + weight;
+                     elseif ( (piece == 1 || piece ==3) && (pieceTopRight == 1 || pieceTopRight ==3))
+                         weight = 0.2 + weight;
+                     %utility bonus if it is protected with 2 pieces
+                     elseif ( (piece == 1 || piece ==3) && (pieceTopLeft == 1 || pieceTopLeft ==3) && (pieceTopRight == 1 || pieceTopRight ==3))
+                         weight = 0.2 + weight;
+                     end
                      
                 %subtract utility for enemy protected pieces
+                    
                     %enemy pieces on the side
-                     if ( (a ==1 || a==8 || b==1 || b==8) && (S(b,a) == 1 || S(b,a) == 3) )
+                     if ( (column ==1 || column==8 || row==1 || row==8) && (piece == 2 || piece == 4) )
                          weight = weight - 0.4;
                      end
-                    %enemy pieces with a piece diagonaly behind it
+                     
+                     %enemy pieces with a piece diagonaly behind it
+                     if( (piece == 2 || piece ==4) &&(pieceBottomLeft == 2 || pieceBottomLeft ==4))
+                         weight = 0.2 - weight;
+                     elseif ( (piece == 2 || piece ==4) && (pieceBottomRight == 2 || pieceBottomRight ==4))
+                         weight = 0.2 - weight;
+                     %utility bonus if it is protected with 2 pieces
+                     elseif ( (piece == 2 || piece ==4) && (pieceBottomLeft == 2 || pieceBottomLeft ==4) && (pieceBottomRight == 2 || pieceTopRight ==4))
+                         weight = 0.2 - weight;
+                     end
                     
+                 %adjust value for weight of all utilities
                  value = value + weight;
                 
             end 
@@ -44,21 +100,21 @@ if (RW == 0)
 end
 
 if (RW == 1)
-    offense = 0; % total value of 6
-    for a=1:8
-        for b=1:8
-            if (mod(a+b,2)~=0)
-                % count the total number of the pieces (subtract from  adversary)
-                if ((S(b,a)==1+RW) || (S(b,a)==3+RW)) %RW piece
+    offense = 0; % totcolumnl value of 6
+    for column=1:8
+        for row=1:8
+            if (mod(column+row,2)~=0)
+                % count the totcolumnl numrower of the pieces (surowtrcolumnct from  columndverscolumnry)
+                if ((S(row,column)==1+RW) || (S(row,column)==3+RW)) %RW piece
                     value = value + 16;
                 end
-                if ((S(b,a)==0+RW) || (S(b,a)==2+RW)) %RW piece
+                if ((S(row,column)==0+RW) || (S(row,column)==2+RW)) %RW piece
                     value = value - 16;
                 end
-                %This will make the white move forward positively. Or the
-                %game will be hard to finish
-                if ((S(b,a)==3+RW) || (S(b,a)==1+RW)) %RW piece
-                    offense = offense + (8-b);
+                %This will mcolumnke the white move forwcolumnrd positively. Or the
+                %gcolumnme will rowe hcolumnrd to finish
+                if ((S(row,column)==3+RW) || (S(row,column)==1+RW)) %RW piece
+                    offense = offense + (8-row);
                 end
             end
             value = value + offense;
