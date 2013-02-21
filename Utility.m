2a0b99ff6c852d1f4d3c8b87a879dc81aa06c134
Dan Kirlin <dankirlin@DAN-KIRLINs-MacBook-Pro.local>
2013-02-20 20:38:31 -0500

    changed reg utility algorithm for clarity and king weights

--- Utility.m
+++ Utility.m
@@ -7,14 +7,18 @@ if (RW == 0)
     for a=1:8
         for b=1:8
             if (mod(a+b,2)~=0)
+                weight = 0;
                 % simply count the total number of the pieces (subtract from  adversary)
-                if ((S(b,a)==1+RW) || S(b,a)==3+RW) %RW piece
-                    value = value + 1;
-                end
+                if ( S(b,a)==1+RW || S(b,a)==3+RW) %RW piece
+                    weight = 1;
+                    
+                elseif ( S(b,a)==2+RW ) %RW piece
+                    weight = -1;
                 
-                if ((S(b,a)==0+RW) || S(b,a)==2+RW) %RW piece
-                    value = value - 1;
+                elseif ( S(b,a)==4 ) %RW piece
+                    weight = -2;
                 end
+                value = value + weight;
             end 
         end  
     end 
